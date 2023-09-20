35c70b1c40cf8de66e82b3509a7829b854e39b40
Nitchinho <122398774+nitchinho@users.noreply.github.com>
2023-09-19 21:49:14 -0300

    Finalizado App portifólio add o botão p/ instagram

--- app-card-pessoal/App.js
+++ app-card-pessoal/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Fundo } from './assets/fundo.jpg'
-import { Foto } from './assets/foto.jpg'
+import { StyleSheet, Text, View, Image, Button, Linking, setOpenInstagram } from 'react-native';
+import  Fundo  from './assets/fundo.png'
+import  Foto  from './assets/foto.png'
+import { useState } from 'react';
 
 export default function App() {
+  const [openInstagram, setOpenInstagram] = useState(false);
+  const onPress = () => {
+    if (Linking.canOpenURL('http://www.instagram.com/nitchinho')) {
+      Linking.openURL('http://www.instagram.com/nitchinho');
+      setOpenInstagram(true);
+    } else {
+      // Mostrar um erro
+    }
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Image style={styles.fundo} source={Fundo} />
+      <Image style={styles.cardtopo} source={Fundo} />
+
+      <View style={styles.carddescricao}>
+        <Image style={styles.foto} source={Foto} />
+        <Text style={styles.titulo}>Rodrigo Almeida</Text>
+        <Text style={styles.paragrafo}>Estudante de análise e desenvolvimento de sistemas, certificado SCRUM PSM I, me aperfeiçoando na linguagem JavaScript e seus frameworks (Node.JS, React, React Active)</Text>
       </View>
-      <View style={styles.card2}>
-      <Image style={styles.foto} source={Foto} />
-      <Text style={styles.titulo}>Rodrigo Almeida</Text>
-      <Text style={styles.paragrafo}>Estudante de análise e desenvolvimento de sistemas, certificado SCRUM PSM I, me aperfeiçoando na linguagem JavaScript e seus frameworks (Node.JS, React, React Active)</Text>
-      </View>
-      <StatusBar style="auto" />
+      <Button title='Meu portiflio' onPress={onPress}/>
     </View>
   );
 }
@@ -23,28 +31,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',    
   },
-  card: {
+  cardtopo: {
     flex: 1,
   },
-  card2: {
+  carddescricao: {
     flex: 1,
+    alignItems: 'center'
   },
-  fundo: {
-    width: 50,
-    height: 50,
-  },
-  foto: {
-    width: 50,
-    height: 50,
+  foto: {    
+    width: 100,
+    height: 100,
+    marginTop: 20,
+    marginBottom: 20,
+    borderRadius: 50,
   },
   titulo: {
+    textAlign: 'center',
     fontSize: 22,
     fontWeight: 'bold',
-    textAlign: 'center',
     margin: 30,
   },
   paragrafo: {
