465f816fce4a524c72e3ff4778e64eb8a3577b0e
Nitchinho <nitchinho007@gmail.com>
2023-09-21 16:56:38 -0300

    Adicionado Topo Titulo e Conteudo sem style

--- app-teste-componentes/App.js
+++ app-teste-componentes/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import estilos from './styleApp';
+import BarraTitulo from './components/BarraTitulo/';
+import Card from './components/Card';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ estilos.container}>
+      <BarraTitulo />
+      <Card/>
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
