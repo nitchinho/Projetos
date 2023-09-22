8c21cf5f5139e9ece58ad37f737c054a5b52fb68
Nitchinho <122398774+nitchinho@users.noreply.github.com>
2023-09-21 21:04:17 -0300

    Finalizando componente Card

--- app-teste-componentes/components/Card/index.js
+++ app-teste-componentes/components/Card/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
-
+import estilos from "./styleCard";
 import Foto from '../../assets/Foto.png'
 
 export default function Card() {
     return (
-        <View>
-            <View>
-                <Image source={Foto} />
-                <Text>Samuel Jackson</Text>
-                <Text>Ator e produtor norte-americano</Text>
+        <View style={estilos.container}>
+            <View style={estilos.box}>
+                <Image style={estilos.fotoCapa} source={Foto} />
+                <Text style={estilos.nome}>Samuel Jackson</Text>
+                <Text style={estilos.paragrafo}>Ator e produtor norte-americano</Text>
             </View>
         </View>
     );
---
+++ app-teste-componentes/components/Card/styleCard.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+const estilos = StyleSheet.create({
+container: {
+    alignItems: 'center',
+    backgroundColor: '#0a1c35',
+    paddingVertical: 20,    
+},
+box:{
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    width: 300,
+},
+fotoCapa: {
+    width: 300,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+},
+nome: {
+    marginTop: 10,
+    fontSize: 22,
+    fontWeight: 'bold',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowColor: '#C0C0C0',
+    textShadowRadius: 1,
+    textDecorationLine: 'underline',
+    
+},
+paragrafo: {
+    margin: 10,
+    fontStyle: 'italic'
+}
+})
+
+export default estilos;
