e0d15363faf168d44318ed1c2f5a4cb6e207659f
Nitchinho <122398774+nitchinho@users.noreply.github.com>
2023-09-25 22:16:13 -0300

    Finalizado Restaurante e estilos do app Paraty

--- app-pousada-paraty/components/Restaurantes/index.js
+++ app-pousada-paraty/components/Restaurantes/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { View, Text, Image, Button } from 'react-native';
+import style from './style'
+
+import Bar1 from '../../assets/restaurantes/restaurante01.png'
+import Bar2 from '../../assets/restaurantes/restaurante02.png'
+import Bar3 from '../../assets/restaurantes/restaurante03.png'
 
 export default function Restaurante(props) {
     return (
-        <View>
-            <Text>Restaurante</Text>
+        <View style={style.container}>
+            <Text style={style.titulo}>Bares e Restaurantes</Text>
+            <Text style={style.descricao}>O prazer da boa comida você encontra em Paraty. Com inúmeras opções de restaurantes, a cidade oferece o melhor da culinária internacional, juntamente à saborosa cozinha caiçara. Confira alguns bares e restaurantes famosos.</Text>
+            <View style={style.cards}>
+                <Image style={style.img} source={Bar1}/>
+                <Text style={style.cardTitulo}>Armazén Mar</Text>
+                <Text>Localização: Rod Rio-Santos</Text>
+            </View>
+            <View style={style.cards}>
+                <Image style={style.img} source={Bar2}/>
+                <Text style={style.cardTitulo}>Bendita's Restaurante</Text>
+                <Text>Localização: Centro Histórico</Text>
+            </View>
+            <View style={style.cards}>
+                <Image style={style.img} source={Bar3}/>
+                <Text style={style.cardTitulo}>Sereira do Mar Pizza-Bar</Text>
+                <Text>Localização: Praia do Jabaquara</Text>
+            </View>
+
         </View>
     )
 }
---
+++ app-pousada-paraty/components/Restaurantes/style.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+const style=StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#e1e3e4',
+        padding: 20,
+    },
+    img: {
+        width: 300,
+        height: 80,
+        marginBottom: 5,
+    },
+    titulo: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    descricao: {
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    cards: {
+        backgroundColor: '#fff',
+        width: 320,
+        marginBottom: 5,
+        padding: 10,
+        borderRadius:5,
+        borderColor: '#979797',
+        borderWidth: 1
+    },
+    cardTitulo: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    }
+})
+
+export default style;
