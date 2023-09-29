09baeb56dba5f08f5ac5a99870dc57e0f7865384
Nitchinho <122398774+nitchinho@users.noreply.github.com>
2023-09-28 21:20:58 -0300

    Adicionado os botões e estilizando

--- app-com-tab-navigation/App.js
+++ app-com-tab-navigation/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, AntDesign } from '@expo/vector-icons';
 
 import TelaInicial from "./components/TelaInicial"
 import TelaSobre from "./components/TelaSobre";
@@ -10,9 +11,34 @@ const Tab = createBottomTabNavigator();
 export default function App(props) {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={TelaInicial} />
-        <Tab.Screen name="Sobre" component={TelaSobre} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelStyle: { fontSize: 14, fontWeight: 'bold', },
+          tabBarStyle: { backgroundColor: 'white' },
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'black',
+          tabBarActiveBackgroundColor: 'black',
+          tabBarLabelPosition: 'beside-icon',
+          headerTitleAlign: 'center',          
+          headerStyle: {
+            backgroundColor: 'white',            
+          },
+          headerBackgroundContainerStyle: false,
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={TelaInicial}
+          options={{
+            tabBarIcon: ({ color }) => (<Entypo name="home" size={24} color={color} />)
+          }} />
+        <Tab.Screen
+          name="Sobre"
+          component={TelaSobre}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="questioncircleo" size={24} color={color} />)
+          }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
