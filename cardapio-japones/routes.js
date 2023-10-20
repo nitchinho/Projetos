e830806052d35cc153ba8827bbbf41bbdbd1e4ab
Nitchinho <nitchinho007@gmail.com>
2023-10-20 17:00:48 -0300

    ADD ROTA ADICIONAL

--- cardapio-japones/routes.js
+++ cardapio-japones/routes.js
@@ -10,11 +10,12 @@ export default function Routes() {
   return (
     <Tab.Navigator screenOptions={options}>
       <Tab.Screen name="Início" component={Inicio} />
+      <Tab.Screen name="Balas" component={Categoria} initialParams={{ categoria: "Balas" }} />
       <Tab.Screen name="Chicletes" component={Categoria} initialParams={{ categoria: "Chicletes" }} />
       <Tab.Screen name="Pirulitos" component={Categoria} initialParams={{ categoria: "Pirulitos" }} />
-      <Tab.Screen name="Balas" component={Categoria} initialParams={{ categoria: "Balas" }} />
-      <Tab.Screen name="Licenciados" component={Categoria} initialParams={{ categoria: "Licenciados" }} />
       <Tab.Screen name="Candy Toys" component={Categoria} initialParams={{ categoria: "Candy Toys" }} />
+      <Tab.Screen name="Licenciados" component={Categoria} initialParams={{ categoria: "Licenciados" }} />
+      <Tab.Screen name="Brinquedos" component={Categoria} initialParams={{ categoria: "Licenciados" }} />
       <Tab.Screen name="Outros" component={Categoria} initialParams={{ categoria: "Outros" }} />      
     </Tab.Navigator>
   );
