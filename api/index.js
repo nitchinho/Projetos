37c875083337dac2e7c5d5d1275874dc9aef6445
Nitchinho <nitchinho007@gmail.com>
2023-10-24 20:15:04 -0300

    API

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+let selecoes = [
+    { nome: 'Brasil' },
+    { nome: 'Estados Unidos' },
+    { nome: 'Japão' },
+    { nome: 'Coréia do Sul' },
+    { nome: 'Argentina' },
+    { nome: 'Alemanhã' },
+    { nome: 'França' },
+    { nome: 'Itália' },
+    { nome: 'Suécia' },
+    { nome: 'Nigéria' }
+];
+
+let data = new Date();
+let dia_mes = data.getDate();
+let mes = data.getMonth();
+
+let selecao;
+
+if (dia_mes < 11) {
+    selecao = selecoes.nome[mes - 1];
+}
+else {
+    selecao = "Não há uma seleção para hoje";
+}
+app.get('/', (req, res) => {
+    res.json({ selecao: selecao });
+});
+
+app.listen(8080, () => {
+    console.log('Servidor node iniciado em: ' + dia_mes + " " +mes);
+})
