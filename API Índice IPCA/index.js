029a9e660e855a7b8fac84eaf9911bb2e43b73d3
Nitchinho <122398774+nitchinho@users.noreply.github.com>
2023-11-01 09:37:40 -0300

    Finalizado projeto

--- API Índice IPCA/index.js
+++ API Índice IPCA/index.js
@@ -3,36 +3,7 @@ import { buscaPorAno, buscaPorId, buscaTodos, calculoReajuste, validaErro } from
 
 const app = express();
 
-app.get('./historicoIPCA', (req, res) => {
-    const ano = parseInt(req.query.ano);
-    if (isNaN(ano)) {
-        res.json(buscaTodos());
-    } else {
-        const resultado = buscaPorAno(ano);
-        if (resultado.length > 0) {
-            res.json(resultado);
-        } else {
-            res.status(404).json({ erro: 'Nenhum histórico para o ano especificado foi encontrado' });
-        }
-    }
-});
-
-app.get('./historicoIPCA/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    if (isNaN(id)) {
-        res.status(404).json({ erro: 'ID Inválido' });
-        return;
-    }
-    const el = buscaPorId(id);
-    if (el) {
-        res.json(el);
-    } else {
-        res.status(404).json({ erro: 'Elemento não encontrado' });
-    }
-});
-
-app.get('./historicoIPCA/calculo', (req, res) => {
+app.get('/historicoIPCA/calculo', (req, res) => {
     const valor = parseFloat(req.query.valor);
     const dataInicioMes = parseInt(req.query.mesInicio);
     const dataInicioAno = parseInt(req.query.anoInicio);
@@ -47,6 +18,36 @@ app.get('./historicoIPCA/calculo', (req, res) => {
     const resultado = calculoReajuste(valor, dataInicioMes, dataInicioAno, dataFinalMes, dataFinalAno);
     res.json({ resultado: resultado });
 });
+
+app.get('/historicoIPCA', (req, res) => {
+    const ano = parseInt(req.query.ano);
+    if (isNaN(ano)) {
+        res.json(buscaTodos());
+    } else {
+        const resultado = buscaPorAno(ano);
+        if (resultado.length > 0) {
+            res.json(resultado);
+        } else {
+            res.status(404).json({ erro: 'Nenhum histórico para o ano especificado foi encontrado' });
+        }
+    }
+});
+
+app.get('/historicoIPCA/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (isNaN(id)) {
+        res.status(404).json({ erro: 'ID Inválido' });
+        return;
+    }
+    const el = buscaPorId(id);
+    if (el) {
+        res.json(el);
+    } else {
+        res.status(404).json({ erro: 'Elemento não encontrado' });
+    }
+});
+
 
 app.listen(8080, () => {
     console.log('Servidor Iniciado na Porta 8080');
